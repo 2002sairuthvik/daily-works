44c2f989c3b5bed7e1bdceec9dbcceb9cd1be5ec
Sai Ruthvik <msairuthvik@gmail.com>
2024-03-14 21:53:45 +0530

    added snake game and learned about inheritance

--- day19.py
+++ day19.py
@@ -1,32 +1,48 @@
-from turtle import Turtle , Screen
+# from turtle import Turtle , Screen
 
 
-tim = Turtle()
-screen = Screen()
+# tim = Turtle()
+# screen = Screen()
 
-def move_forwards():
-    tim.forward(10)
+# def move_forwards():
+#     tim.forward(10)
     
-def move_backwards():
-    tim.backward(10)
+# def move_backwards():
+#     tim.backward(10)
     
-def turn_left():
-    new_heading = tim.heading()+10
-    tim.setheading(new_heading)
+# def turn_left():
+#     new_heading = tim.heading()+10
+#     tim.setheading(new_heading)
     
-def turn_right():
-    new_heading = tim.heading()-10
-    tim.setheading(new_heading)
+# def turn_right():
+#     new_heading = tim.heading()-10
+#     tim.setheading(new_heading)
     
-def clear():
-    tim.clear()
-    tim.penup()
-    tim.home()
+# def clear():
+#     tim.clear()
+#     tim.penup()
+#     tim.home()
     
-screen.listen()
-screen.onkey(move_forwards , 'w')
-screen.onkey(move_backwards , 's')
-screen.onkey(turn_left , 'a')
-screen.onkey(turn_right , 'd')
-screen.onkey(clear , 'c')
-screen.exitonclick()
+# screen.listen()
+# screen.onkey(move_forwards , 'w')
+# screen.onkey(move_backwards , 's')
+# screen.onkey(turn_left , 'a')
+# screen.onkey(turn_right , 'd')
+# screen.onkey(clear , 'c')
+# screen.exitonclick()
+class Dog:
+    def __init__(self):
+        self.temperament = "loyal"
+
+class Labrador(Dog):
+    def __init__(self):
+        super().__init__()
+        self.temperament = "gentle"
+
+
+
+doggo = Dog()
+print(f"A dog is {doggo.temperament}")
+
+sparky = Labrador()
+print(f"Sparky is {sparky.temperament}")
